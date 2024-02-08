7c2cbad560d60c1d1b951eaeb9923a3aa0134ad6
k-46 <kamalxsh31@gmail.com>
2024-02-08 15:44:36 +0530

    connection database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,47 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+const mongoose = require("mongoose");
 
-app.get("/ping", (req, res) => {
-  res.json({message : "pong"});
+let connectionStatus = "Disconnected";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    connectionStatus = "Connected";
+  } catch (err) {
+    connectionStatus = "Failed";
+    console.log(err);
+  }
+};
+
+const disconnectDB = async () => {
+  try {
+    await mongoose.disconnect();
+    connectionStatus = "Closed";
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "MongoDB",
+    dataBase: connectionStatus,
+  });
 });
 
-app.listen(port, () => {
-  console.log(`server running on PORT: ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`server running on PORT: ${process.env.PORT}`);
+});
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
 });
 
 module.exports = app;
