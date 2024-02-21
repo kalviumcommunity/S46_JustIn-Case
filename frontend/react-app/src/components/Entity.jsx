faae6ac8d1d2c07d00c941671287c62184e73a5d
k-46 <kamalxsh31@gmail.com>
2024-02-21 12:23:14 +0530

    css changes

--- frontend/react-app/src/components/Entity.jsx
+++ frontend/react-app/src/components/Entity.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import { AiFillHeart } from "react-icons/ai";
 import { BiSolidDislike } from "react-icons/bi";
 
-
 const Entity = () => {
-    console.log(AiFillHeart)
   return (
     <div className="container">
-      <p>
-      I told my wife she should embrace her mistakes. She gave me a hug.
-      </p>
+      <p>I told my wife she should embrace her mistakes. She gave me a hug.</p>
       <div className="like-dis">
-      <AiFillHeart size='30px' fill="grey"/>
-      <BiSolidDislike size='30px' fill="grey"  />
+        <span className="likes">
+          <AiFillHeart size="30px" fill="grey" />
+          <p>10</p>
+        </span>
+        <span className="disLikes">
+          <BiSolidDislike size="30px" fill="grey" />
+          <p>10</p>
+        </span>
       </div>
     </div>
   );
