41698116097ab7f414b38f333c5e7e9bdc9be771
k-46 <kamalxsh31@gmail.com>
2024-02-26 13:54:09 +0530

    Create post method in React

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -1,12 +1,44 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import AllRoutes from "./components/AllRoutes";
 import "./App.css";
-import Header from "./components/Header";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
+export const Details = React.createContext();
 function App() {
+  const navigate = useNavigate();
+  const [postDetails, setPostDetails] = useState([]);
+  const api = "https://justin-case.onrender.com/api/posts";
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        await axios
+          .get(api)
+          .then((res) => {
+            setPostDetails(res.data);
+          })
+          .catch((err) => console.log(err));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getData();
+    console.log("App.jssx");
+  }, []);
+
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <>
+      <header>
+        <div className="logo-sign">
+          <h1 onClick={() => navigate("/")}>JustIn Case</h1>
+          <button id="logbtn">Login/SignUp</button>
+        </div>
+      </header>
+      <Details.Provider value={{ postDetails, setPostDetails }}>
+        <AllRoutes />
+      </Details.Provider>
+    </>
   );
 }
 
